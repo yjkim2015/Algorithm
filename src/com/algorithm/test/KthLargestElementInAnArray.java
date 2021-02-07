b135879abe5860e71aa849f5c6aaa36954a482f3
yjkim <forteas2003@hanmail.net>
2021-02-08 00:36:21 +0900

    [김윤제] KthLargestElementInAnArray

---
+++ src/com/algorithm/test/KthLargestElementInAnArray.java
@@ -0,0 +1,18 @@
+package com.algorithm.test;
+
+
+/**
+ * 
+ * @author forte
+ *	int[] nums  = {3,2,1,5,6,4}
+ *	int k 		= 2;
+ *	Output 		= 5;
+ * k번째 큰 수 구하기 
+ * 
+ * Solution
+ * 1. 배열 오름차순 {1,2,3,4,5,6} 6-2 = 4
+ * 2. PriorityQueue, k = 2
+ */
+public class KthLargestElementInAnArray {
+
+}
