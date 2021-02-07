e1928c52d1d7e0224f05cf05ba1cc8d73c05ba07
yjkim <forteas2003@hanmail.net>
2021-02-08 00:11:33 +0900

    [김윤제] TrapppingRainWater 부연 설명 추가

--- src/com/algorithm/test/TrappingRainWater.java
+++ src/com/algorithm/test/TrappingRainWater.java
@@ -5,6 +5,17 @@ package com.algorithm.test;
 // right
 // max
 // min
+/*
+ * Example
+ * 1. int[] left 	= {0,1,1,2,2,2,2,3,3,3,3,3}
+ * 2. int[] right 	= {3,3,3,3,3,3,3,3,2,2,2,1}
+ * 3. Math.min  	= {0,1,1,2,2,2,2,3,2,2,2,1}
+ * 4. Height		= {0,1,0,2,1,0,1,3,2,1,2,1}
+ * 5.				= {0,0,1,0,1,2,1,0,0,1,0,0}
+ * 
+ * 1. Math.min(left[i],right[i])-height[i];
+ * left,right를 구해서 가장 작은 값을 구한 뒤 가지고있는 높이만큼 뺀다.
+ */
 public class TrappingRainWater {
 
 }
